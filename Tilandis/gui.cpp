fa0584e91d13addce918d739cd4f85eaa1351992
Nicholas O'Connor <nrfoconnor@gmail.com>
2016-01-27 13:43:55 -0800

    cleanup

--- Tilandis/gui.cpp
+++
@@ -1,9 +0,0 @@
-#include "gui.h"
-
-bool GUI::InitMFC() { //stub
-	GUI::RegisterProtocol->Create(IDD_REGISTER_PROTOCOL);
-}
-
-bool GUI::ShowDialog(CDialog * tgt) {
-	tgt->ShowWindow(SW_SHOWDEFAULT); // TODO: error checking
-}
