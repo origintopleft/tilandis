5c1e69f8866e9d498f794cbdecb9e83a3680a79b
Nicholas O'Connor <nrfoconnor@gmail.com>
2016-03-29 14:26:35 -0700

    hooked up the port box

--- TilandisUWP/Settings.xaml.cs
+++ TilandisUWP/Settings.xaml.cs
@@ -26,6 +26,7 @@ namespace TilandisUWP
 
             edt_launchprotocol.Text = (string) App.adc_local.Values["str_launchprotocol"];
             edt_ctlprotocol.Text = (string) App.adc_local.Values["str_controlprotocol"];
+            edt_resport.Text = ((int) App.adc_local.Values["int_port"]).ToString();
 
             cbx_darkui.IsChecked = (bool) App.adc_local.Values["bool_darkui"];
         }
@@ -39,6 +40,27 @@ namespace TilandisUWP
 
         private void clk_darkuibox(object sender, RoutedEventArgs e) {
             App.adc_local.Values["bool_darkui"] = cbx_darkui.IsChecked;
+        }
+
+        private void clbk_port_edited(object sender, TextChangedEventArgs e) {
+            int result;
+            if (int.TryParse(edt_resport.Text, out result)) {
+                edt_resport.Text = result.ToString(); // Just in case there were letters and we just stripped them
+
+                if (result > 65535) {
+                    // Invalid port number
+                    Random rng = new Random();
+                    result = rng.Next(50000, 60000);
+                    edt_resport.Text = result.ToString();
+                }
+            } else {
+                // Bad text
+                Random rng = new Random();
+                result = rng.Next(50000, 60000);
+                edt_resport.Text = result.ToString();
+            }
+
+            App.adc_local.Values["int_port"] = result;
         }
     }
 }
